60a35c79b32f4656a8bcd6dba70d92c36834ae85
pgibler <pgibler@0bfe2625-df30-48dd-956e-b14655f1835e>
2009-12-01 08:05:17 +0000

    Removed use of AsyncTask. Just load image synchronously.
    git-svn-id: https://pgibler.unfuddle.com/svn/pgibler_magicval/MagicVal@18 0bfe2625-df30-48dd-956e-b14655f1835e

--- src/com/magicval/DisplaySingleCardActivity.java
+++ src/com/magicval/DisplaySingleCardActivity.java
@@ -9,43 +9,83 @@ import com.magicval.R;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplaySingleCardActivity extends Activity {
 	
+	//private ProgressDialog pd;
+	private FrameLayout frame;
+	private DisplaySingleCardActivity ref;
+	private Card card;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_single_card);
         
+        ref = this;
+        
         Bundle b = getIntent().getExtras();
         
-        Card c = b.getParcelable("Card");
-        MonetaryValue v = c.getMonetaryValue();
+        frame = (FrameLayout) findViewById(R.id.DisplaySingleCardImageFrameLayout);
         
-        setTxt(R.id.DisplaySingleCardNameText, c.getNameFromSearch());
-        setTxt(R.id.DisplaySingleCardCurrentPriceTextView, v.getCurrentPriceAsCurrency());
-        setTxt(R.id.DisplaySingleCardHighPriceTextView, v.getHighPriceAsCurrency());
-        setTxt(R.id.DisplaySingleCardLowPriceTextView, v.getLowPriceAsCurrency());
+        View v = generateView(b);
+        renderView(v);
         
-        FrameLayout frame = (FrameLayout) findViewById(R.id.DisplaySingleCardImageFrameLayout);
+        // TODO Properly use AsyncTask to load the image.
         
-        try {
+        //pd = ProgressDialog.show(this, "Rendering", "Rendering card data", true,false);
+        //new DisplaySingleCardImageTask().execute(b);
+		//pd.dismiss();
+	}
+	
+	/*class DisplaySingleCardImageTask extends AsyncTask<Bundle, Void, View>
+	{
+
+		@Override
+		protected View doInBackground(Bundle... params) {
+			return generateView(params[0]);
+		}
+		
+		@Override
+		protected void onPostExecute(View result) {
+			pd.dismiss();
+			renderView(result);
+		}
+		
+	}*/
+	
+	private View generateView(Bundle b) {
+		View v;
+		try {
+			card = b.getParcelable("Card");
         	// TODO Make it so that if the user clicks on the card image,
         	// it expands to the center of the screen for easier viewing.
-        	Bitmap cardImage = c.getImage();
-        	ImageView image = new ImageView(this);
+        	Bitmap cardImage = card.getImage();
+        	ImageView image = new ImageView(ref);
             int val = 15;
             image.setPadding(val, val, val, val);
 			image.setImageBitmap(cardImage);
-	        frame.addView(image);
+	        v = image;
 		} catch (IOException e) {
-			TextView text = new TextView(this);
+			TextView text = new TextView(ref);
 			text.setText("No image available for this card");
-			frame.addView(text);
+			v = text;
 		}
+		return v;
+	}
+	
+	private void renderView(View result) {
+		MonetaryValue v = card.getMonetaryValue();
+		setTxt(R.id.DisplaySingleCardNameText, card.getNameFromSearch());
+        setTxt(R.id.DisplaySingleCardCurrentPriceTextView, v.getCurrentPriceAsCurrency());
+        setTxt(R.id.DisplaySingleCardHighPriceTextView, v.getHighPriceAsCurrency());
+        setTxt(R.id.DisplaySingleCardLowPriceTextView, v.getLowPriceAsCurrency());
+		frame.addView(result);
+		
 	}
 	
 	private void setTxt(int viewid, String txt)
